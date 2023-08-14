9089f266adb929bf1ed766008f0edb763c8c1627
Juanma Hidalgo <juanma06@gmail.com>
2023-08-14 17:10:30 -0300

    fix: add missing param pickedBy read from auth context

--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -1,7 +1,10 @@
 import { Router } from '@well-known-components/http-server'
+import * as authorizationMiddleware from 'decentraland-crypto-middleware'
 import { GlobalContext } from '../types'
 import { createCatalogHandler } from './handlers/catalog-handler'
 import { pingHandler } from './handlers/ping-handler'
+
+const FIVE_MINUTES = 5 * 60 * 1000
 
 // We return the entire router because it will be easier to test than a whole server
 export async function setupRouter(globalContext: GlobalContext): Promise<Router<GlobalContext>> {
@@ -9,7 +12,14 @@ export async function setupRouter(globalContext: GlobalContext): Promise<Router<
   const router = new Router<GlobalContext>()
 
   router.get('/ping', pingHandler)
-  router.get('/v1/catalog', createCatalogHandler(components))
+  router.get(
+    '/v1/catalog',
+    authorizationMiddleware.wellKnownComponents({
+      optional: true,
+      expiration: FIVE_MINUTES
+    }),
+    createCatalogHandler(components)
+  )
 
   return router
 }
