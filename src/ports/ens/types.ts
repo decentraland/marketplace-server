05d0b6556f8975f67939ebb3c500e9e95b878607
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2024-01-19 12:47:38 -0300

    feat: accept getting ens image with just the logo (#79)

--- src/ports/ens/types.ts
+++ src/ports/ens/types.ts
@@ -1,5 +1,5 @@
 import { PNGStream } from 'canvas'
 
 export type IENSComponent = {
-  generateImage(ens: string, width: number, height: number): Promise<PNGStream>
+  generateImage(ens: string, width: number, height: number, onlyLogo?: boolean): Promise<PNGStream>
 }
