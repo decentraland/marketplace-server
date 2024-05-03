02fc762d2054e1d2bda868a57a6b6d11054a9881
Juanma Hidalgo <juanma06@gmail.com>
2024-05-03 15:58:34 +0200

    test: update test components file

--- test/components.ts
+++ test/components.ts
@@ -1,19 +1,26 @@
 // This file is the "test-environment" analogous for src/components.ts
 // Here we define the test components to be used in the testing environment
-import path from 'path'
 import { createDotEnvConfigComponent } from '@well-known-components/env-config-provider'
 import { createServerComponent } from '@well-known-components/http-server'
 import { createLogComponent } from '@well-known-components/logger'
 import { createMetricsComponent } from '@well-known-components/metrics'
-import { createPgComponent, IPgComponent } from '@well-known-components/pg-component'
+import { IPgComponent } from '@well-known-components/pg-component'
 import { createRunner, createLocalFetchCompoment } from '@well-known-components/test-helpers'
+import { createTracerComponent } from '@well-known-components/tracer-component'
 import { createFetchComponent } from '../src/adapters/fetch'
 import { metricDeclarations } from '../src/metrics'
 import { createBalanceComponent } from '../src/ports/balance/component'
 import { createCatalogComponent } from '../src/ports/catalog/component'
+import { createPgComponent } from '../src/ports/db/component'
 import { createENS } from '../src/ports/ens/component'
+import { createAccessComponent } from '../src/ports/favorites/access'
 import { createFavoritesComponent } from '../src/ports/favorites/components'
+import { createItemsComponent } from '../src/ports/favorites/items'
+import { createListsComponent } from '../src/ports/favorites/lists'
+import { createPicksComponent } from '../src/ports/favorites/picks'
+import { createSnapshotComponent } from '../src/ports/favorites/snapshot'
 import { createJobComponent } from '../src/ports/job'
+import { createSchemaValidatorComponent } from '../src/ports/schema-validator'
 import { createWertSigner } from '../src/ports/wert-signer/component'
 import { main } from '../src/service'
 import { GlobalContext, TestComponents } from '../src/types'
@@ -36,24 +43,23 @@ async function initComponents(): Promise<TestComponents> {
     origin: await config.requireString('CORS_ORIGIN'),
     methods: await config.requireString('CORS_METHODS')
   }
-  const databaseUrl = (await config.getString('PG_COMPONENT_PSQL_CONNECTION_STRING')) || ''
-  const schema = await config.requireString('PG_COMPONENT_PSQL_SCHEMA')
-  const fetch = await createFetchComponent()
+  const tracer = createTracerComponent()
+  const fetch = await createFetchComponent({ tracer })
   const metrics = await createMetricsComponent(metricDeclarations, { config })
   const logs = await createLogComponent({ metrics })
   const server = await createServerComponent<GlobalContext>({ config, logs }, { cors })
 
-  const database = await createPgComponent(
-    { logs, config, metrics },
+  const substreamsDatabase = await createPgComponent(
+    { config, logs, metrics },
     {
-      migration: {
-        databaseUrl,
-        schema,
-        dir: path.resolve(__dirname, '../src/migrations'),
-        migrationsTable: 'pgmigrations',
-        ignorePattern: '.*\\.map',
-        direction: 'up'
-      }
+      dbPrefix: 'SUBSTREAMS'
+    }
+  )
+
+  const favoritesDatabase = await createPgComponent(
+    { config, logs, metrics },
+    {
+      dbPrefix: 'FAVORITES'
     }
   )
 
@@ -61,15 +67,29 @@ async function initComponents(): Promise<TestComponents> {
 
   const favoritesComponent = createFavoritesComponent({ fetch }, MARKETPLACE_FAVORITES_SERVER_URL)
   const SEGMENT_WRITE_KEY = await config.requireString('SEGMENT_WRITE_KEY')
-  const catalog = await createCatalogComponent({ database, favoritesComponent }, SEGMENT_WRITE_KEY)
   const COVALENT_API_KEY = await config.getString('COVALENT_API_KEY')
-  const balances = createBalanceComponent({ apiKey: COVALENT_API_KEY ?? '' })
   const WERT_PRIVATE_KEY = await config.requireString('WERT_PRIVATE_KEY')
   const WERT_PUBLICATION_FEES_PRIVATE_KEY = await config.requireString('WERT_PUBLICATION_FEES_PRIVATE_KEY')
   const wertSigner = createWertSigner({ privateKey: WERT_PRIVATE_KEY, publicationFeesPrivateKey: WERT_PUBLICATION_FEES_PRIVATE_KEY })
   const ens = createENS()
+
+  // favorites stuff
+  const snapshot = await createSnapshotComponent({ fetch, config })
+  const items = createItemsComponent({ logs, substreamsDatabase })
+  const lists = createListsComponent({
+    favoritesDatabase,
+    items,
+    snapshot,
+    logs
+  })
+  const access = createAccessComponent({ favoritesDatabase, logs, lists })
+  const picks = createPicksComponent({ favoritesDatabase, items, snapshot, logs, lists })
+  const catalog = await createCatalogComponent({ substreamsDatabase, picks }, SEGMENT_WRITE_KEY)
+  const schemaValidator = await createSchemaValidatorComponent()
+  const balances = createBalanceComponent({ apiKey: COVALENT_API_KEY ?? '' })
   // Mock the start function to avoid connecting to a local database
-  jest.spyOn(database, 'start').mockResolvedValue(undefined)
+  jest.spyOn(substreamsDatabase, 'start').mockResolvedValue(undefined)
+  jest.spyOn(favoritesDatabase, 'start').mockResolvedValue(undefined)
   jest.spyOn(catalog, 'updateBuilderServerItemsView').mockResolvedValue(undefined)
   const updateBuilderServerItemsViewJob = createJobComponent({ logs }, () => catalog.updateBuilderServerItemsView(), 5 * 60 * 1000, {
     startupDelay: 30
@@ -82,13 +102,20 @@ async function initComponents(): Promise<TestComponents> {
     localFetch: await createLocalFetchCompoment(config),
     fetch,
     metrics,
-    database,
+    substreamsDatabase,
+    favoritesDatabase,
     catalog,
     favoritesComponent,
     balances,
     wertSigner,
     ens,
-    updateBuilderServerItemsViewJob
+    updateBuilderServerItemsViewJob,
+    access,
+    lists,
+    picks,
+    snapshot,
+    items,
+    schemaValidator
   }
 }
 
