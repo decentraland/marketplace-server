2bea35fba559da2728fbbf4845b32db67b268576
Lautaro Petaccio <lausuper@gmail.com>
2024-05-15 17:46:46 -0300

    fix: Remove created at from materialized view

--- src/migrations/substreams/1715105480588_builder-server-builder-item-utility.ts
+++ src/migrations/substreams/1715105480588_builder-server-builder-item-utility.ts
@@ -12,14 +12,12 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
     { ifNotExists: true },
     `SELECT
       collections.contract_address || '-' || server_items.blockchain_item_id AS item_id,
-      server_items.utility as utility,
-      blockchain_items.created_at
+      server_items.utility as utility
      FROM
-      builder_server_items server_items, builder_server_collections collections, dcl36.items blockchain_items
+      builder_server_items server_items, builder_server_collections collections
      WHERE server_items.blockchain_item_id IS NOT NULL 
      AND server_items.utility IS NOT NULL 
-     AND server_items.collection_id = collections.id 
-     AND blockchain_items.id = (collections.contract_address || '-' || server_items.blockchain_item_id);`
+     AND server_items.collection_id = collections.id;`
   )
   pgm.addIndex(materializedViewName, ['item_id'], { name: 'idx_mv_builder_server_items_utility', unique: true, ifNotExists: true })
 }
