68aab93ec495cc889266b63be056e38131021232
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2024-06-18 14:57:27 -0300

    feat: move metadata validation to crypto middleware

---
+++ src/controllers/utils.ts
@@ -0,0 +1,8 @@
+import RequestError from 'decentraland-crypto-middleware/lib/errors'
+
+export function validateAuthMetadataSigner(metadata: Record<string, any> | undefined) {
+  if (metadata && metadata.signer === 'decentraland-kernel-scene') {
+    throw new RequestError('Invalid signer', 400)
+  }
+  return true
+}
