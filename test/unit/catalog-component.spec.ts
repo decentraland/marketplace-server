68e760828f9d82d8f8232c32bb2c1d9fcad685ec
Juanma Hidalgo <juanma06@gmail.com>
2024-12-11 23:17:43 +0100

    fix: test

--- test/unit/catalog-component.spec.ts
+++ test/unit/catalog-component.spec.ts
@@ -12,6 +12,7 @@ jest.mock('@segment/analytics-node')
 
 let catalogComponent: ICatalogComponent
 let dappsDatabase: AppComponents['dappsDatabase']
+let dappsWriteDatabase: AppComponents['dappsDatabase']
 let picks: IPicksComponent
 let dbClientQueryMock: jest.Mock
 let dbClientReleaseMock: jest.Mock
@@ -38,7 +39,7 @@ beforeEach(async () => {
   }
 
   segmentWriteKey = 'testSegmentWriteKey'
-  catalogComponent = await createCatalogComponent({ dappsDatabase, picks }, segmentWriteKey)
+  catalogComponent = await createCatalogComponent({ dappsDatabase, dappsWriteDatabase, picks }, segmentWriteKey)
 })
 
 describe('Catalog Component', () => {
