31e2519451bda446b826c09d6833998cea5a9925
Juanma Hidalgo <juanma06@gmail.com>
2024-12-17 18:01:33 +0100

    fix: sort the lands by recently listed correctly

--- src/logic/date.ts
+++ src/logic/date.ts
@@ -2,6 +2,10 @@ export function fromMillisecondsToSeconds(timeInMilliseconds: number): number {
   return Math.floor(timeInMilliseconds / 1000)
 }
 
-export function fromSecondsToMilliseconds(timeInSeconds: number): number {
-  return timeInSeconds * 1000
+// it rounds the time to the nearest second since the trades are stored with a precision of milliseconds already
+export function fromSecondsToMilliseconds(time: number): number {
+  if (time.toString().length <= 10) {
+    return Math.round(time * 1000)
+  }
+  return Math.round(time)
 }
