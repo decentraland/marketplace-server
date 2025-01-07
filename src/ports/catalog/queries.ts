e76e670f3e136851fc125f23ba1f228d04059427
Juanma Hidalgo <juanma06@gmail.com>
2025-01-07 11:48:57 +0100

    feat: use expires_at_normalized field

--- src/ports/catalog/queries.ts
+++ src/ports/catalog/queries.ts
@@ -696,7 +696,7 @@ export const getCollectionsItemsCatalogQueryWithTrades = (filters: CatalogQueryF
             SQL`.order AS orders 
             WHERE 
                 orders.status = 'open' 
-                AND orders.expires_normalized > NOW() `
+                AND orders.expires_at_normalized > NOW() AND nft.owner_address = orders.owner`
           )
       )
       .append(getOrderRangePriceWhere(filters))
