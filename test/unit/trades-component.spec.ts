11bb930b2901dcd188e95006ee7b998ff96f0f2e
LautaroPetaccio <lausuper@gmail.com>
2026-04-23 19:40:22 -0300

    fix: mock preceding validations in signature length test

--- test/unit/trades-component.spec.ts
+++ test/unit/trades-component.spec.ts
@@ -155,6 +155,8 @@ describe('when adding a new trade', () => {
   describe('when the trade signature length is not 132 characters', () => {
     beforeEach(() => {
       mockTrade.signature = '0xshort'
+      jest.spyOn(utils, 'validateTradeByType').mockResolvedValue(true)
+      jest.spyOn(utils, 'isValidEstateTrade').mockResolvedValueOnce(true)
     })
 
     it('should throw an InvalidTradeSignatureError', async () => {
